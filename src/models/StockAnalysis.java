1f8b06af71c49daed853f0e1dc291f20251e75ad
Andres Pineda <andres@pineda.ws>
2016-04-28 18:42:41 -0400

    Updated StockAnalysis object

--- src/models/StockAnalysis.java
+++ src/models/StockAnalysis.java
@@ -1,5 +1,26 @@
 package models;
 
+import yahoofinance.Stock;
+
 public class StockAnalysis extends AAnalysisObject {
 
+	private boolean _outcome;
+	private boolean _previousDayGained;
+	private boolean _volumeGreaterThanAvgVolume;
+	
+	public StockAnalysis() {
+		super();
+	}
+	
+	public StockAnalysis(final Stock stock) {
+		super();
+	}
+	
+	public boolean getOutcome() {
+		return _outcome;
+	}
+	
+	public void setOutcome(final boolean outcome) {
+		_outcome = outcome;
+	}
 }
