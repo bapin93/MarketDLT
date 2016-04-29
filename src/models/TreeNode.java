0a9ebd2350352581d8317d5f1f55aaffa59a0f5c
Andres Pineda <andres@pineda.ws>
2016-04-29 17:59:28 -0400

    Further developed TreeNode

--- src/models/TreeNode.java
+++ src/models/TreeNode.java
@@ -6,14 +6,43 @@ public class TreeNode {
 	
 	private TreeNode _parent;
 	private List<TreeNode> _children;
-	private List<StockAnalysis> _analysisList;
+	private List<AAnalysisObject> _analysisList;
 	
 	public TreeNode() {
 		
 	}
 	
-	public TreeNode(final List<StockAnalysis> analysisList) {
+	public TreeNode(final List<AAnalysisObject> analysisList) {
 		_analysisList = analysisList;
 	}
 	
+	public TreeNode(final TreeNode parent, final List<AAnalysisObject> analysisList) {
+		_parent = parent;
+		_parent.addChild(this);
+		_analysisList = analysisList;
+	}
+	
+	public TreeNode getParent() {
+		return _parent;
+	}
+	
+	public void setParent(final TreeNode parent) {
+		_parent = parent;
+	}
+	
+	public List<TreeNode> getChildren() {
+		return _children;
+	}
+	
+	public void setChildren(final List<TreeNode> children) {
+		_children = children;
+	}
+	
+	public void addChild(final TreeNode child) {
+		_children.add(child);
+	}
+	
+	public List<AAnalysisObject> getAnalysisList() {
+		return _analysisList;
+	}
 }
