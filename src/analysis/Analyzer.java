bb4f32c860d93b940426274fa6a71c8ff26d08b8
Andres Pineda <andres@pineda.ws>
2016-04-29 17:58:07 -0400

    Added Analyzer class

---
+++ src/analysis/Analyzer.java
@@ -0,0 +1,16 @@
+package analysis;
+
+import models.TreeNode;
+
+public class Analyzer implements IAnalyzer {
+	
+	public Analyzer() {
+		
+	}
+	
+	public Analyzer(final TreeNode node) {
+		
+	}
+	
+	
+}
