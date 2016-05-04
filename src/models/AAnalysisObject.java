c1b92bd35ad89da8b36d74230a420edbee38b3d3
Andres Pineda <andres@pineda.ws>
2016-05-04 16:55:20 -0400

    added to AAnalysisObject

--- src/models/AAnalysisObject.java
+++ src/models/AAnalysisObject.java
@@ -7,6 +7,20 @@ package models;
  * @author andres
  *
  */
-public abstract class AAnalysisObject {	
+public abstract class AAnalysisObject {
+	
+	private boolean _outcome;
+	
+	public AAnalysisObject() {
+		
+	}
+	
+	public boolean getOutcome() {
+		return _outcome;
+	}
+	
+	public void setOutcome(final boolean outcome) {
+		_outcome = outcome;
+	}
 	
 }
