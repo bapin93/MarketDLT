0d8c5f60c2da389b646222cec282e0626d79791c
Andres <andres@iMac.attlocal.net>
2016-05-19 11:04:27 -0500

    Removed old structure files

--- src/utilities/StockAnalysisAttribute.java
+++
@@ -1,7 +0,0 @@
-package utilities;
-
-public enum StockAnalysisAttribute {
-	DAY_GAINED,
-	VOLUME_GREATER_THAN_AVG_VOL,
-	GAINED_MORE_THAN_1_PERCENT
-}
--- src/utilities/Utilities.java
+++
@@ -1,7 +0,0 @@
-package utilities;
-
-import java.util.List;
-
-public class Utilities {
-	
-}
